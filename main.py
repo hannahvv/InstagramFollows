0f5dee3c827554ba75785856f360d5ff6c32148d
Hannah Vaccaro <hannahvaccaro@Hannahs-MBP.lan>
2022-12-20 17:01:05 -0500

    created program that prints a set of usernames that don't follow the user back

---
+++ main.py
@@ -0,0 +1,33 @@
+import json
+
+file_followers = "followers.json"
+file_following = "following.json"
+
+data_followers = open(file_followers)
+data_following = open(file_following)
+
+following = json.load(data_following)
+followers = json.load(data_followers)
+
+
+following_list = set()
+follower_list = set()
+
+#parse through following.json to get following username
+for relationships_following in following['relationships_following']:
+    for data in relationships_following['string_list_data']:
+         following_username = data['value']
+         following_list.add(following_username)
+
+
+#parse through following.json to get followers username
+for relationships_followers in followers['relationships_followers']:
+    for data2 in relationships_followers['string_list_data']:
+        follower_username = data2['value']
+        follower_list.add(follower_username)
+
+
+print(following_list.difference(follower_list))
+
+
+
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Instagram Follows
+
+A simple program that uses a user's personal instagram data specifically the user's
+following and followers data and prints out people who the user's follows but the people don't
+follow the user back.
+
+
+To use the program, the user must log in and download their instagram's 
+[data](https://help.instagram.com/181231772500920) and extract the _following.json_ and _followers.json_
+from the _followers_and_following_ folder.
